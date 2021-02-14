e3fce5eeaad5d9746c030e772a31f8d6843bb7cb
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-02-14 18:50:04 +0300

    Add chi-square test

--- lcg.c
+++ lcg.c
@@ -2,10 +2,15 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <time.h>
+#include <math.h>
 #include <malloc.h>
+
+#define TEST_LIMIT 1000000
+#define TEST_INTERVALS_NUMBER 20
 
 void testLCGParameters();
 unsigned gcd(unsigned long long a, unsigned long long b);
+double chiSquaredTest(size_t intervalsNumber, double mid_value);
 
 static unsigned long long nextValue = 1;
 const unsigned multiplier = 1103515245;
@@ -24,6 +29,7 @@ unsigned nextStep() {
 void main(void) {
 	initLCG();
 	testLCGParameters();
+	chiSquaredTest(TEST_INTERVALS_NUMBER, TEST_LIMIT / TEST_INTERVALS_NUMBER);
 }
 
 void testLCGParameters() {
@@ -43,4 +49,23 @@ unsigned gcd(unsigned long long a, unsigned long long b){
 		b = temp;
 	}
 	return a;
+}
+
+double chiSquaredTest(size_t intervalsNumber, double mid_value) {
+	unsigned currentValue = 0;
+	size_t currentInterval, i;
+	size_t* measurementsNumberInInterval = (size_t*)malloc(intervalsNumber * sizeof(size_t));
+	for (i = 0; i < intervalsNumber; i++) measurementsNumberInInterval[i] = 0;
+	for (i = 0; i < TEST_LIMIT; i++) {
+		currentValue = nextStep();
+		currentInterval = ((float) currentValue / module) * intervalsNumber;
+		measurementsNumberInInterval[currentInterval]++;
+	}
+
+	double xi2 = 0;
+	for (i = 0; i < intervalsNumber; i++) {
+		xi2 += pow(measurementsNumberInInterval[i] - mid_value, 2) / mid_value;
+	}
+
+	printf("chi-squared value: %g\n\n", xi2);
 }
